a77f05cde232faabac1983d37557326836967a4a
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-12-08 09:24:32 +0300

    add task

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -37,32 +37,66 @@ return {
    }
  };
 
+const addTask = async ()=>{
+  try {
+    const res = await axios.post(`${process.env.REACT_APP_BASE_URL}/task`,{
+      task
+    },{
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    dispatch(postTask(res.data))
+  }catch(error){
+    console.log(error);
+  }
+}
+
+
+
+
+
+
+
 
   return (
     <>
       <div>
         {!state.token ? (
+          <div>
           <h2>
             {" "}
             You Must <Link to="/">LogIn </Link> or{" "}
             <Link to="/signup"> Sign Up </Link> To Show Your List{" "}
           </h2>
-        ) : (
-          <div className="wrapper">
+           </div>
+        ) :(
+          <div>
             <input
-              type="text"
-              placeholder="Task ... "
-              onChange={(e) => setTask(e.target.value)}
+            type="text"
+            placeholder=" Task...."
+            onChange={(e)=> setTask(e.target.value)}
             />
-            <button onClick={()=> addTask()}>Add</button>
-             
-             {state.list.length ?(
-               
+            <button onClick={()=> addTask()}> Add</button>
 
-             ) }
-        )}
+          </div>
+          {state.list.length ? (
+            <ul>
+            {state.list.map((task)=>(
+              <div className="item" key={task._id}>
+            <li> {task.task}</li>
+            <button onClick={()=> edit(task._id)}>Edit</button>
+            <button onClick={()=> remove(task._id)}>Remove</button>
+            </div>
+            ))}
+            
+            </ul>
+          ): (
+            <h2> You Don't Have Any Tasks Yet !</h2>
+          )}
+        )
         </div>
-      </div>
+    
     </>
   );
   
